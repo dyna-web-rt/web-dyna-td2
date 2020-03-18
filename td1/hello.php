6644dc88f25b416720ee87bc1a88c3f17601b052
MaelFlamant <62329992+MaelFlamant@users.noreply.github.com>
2020-03-18 11:52:29 +0100

    PHP
    LOL

--- td1/hello.php
+++ td1/hello.php
@@ -1,7 +1,66 @@
 <?php
 echo 'Hello world!';
 ?>
+a
 <hr>
 <?php
 $version = phpversion();
 echo "<h1>php $version is great!</h1>";
+
+// Exo 1 
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hello</title>
+</head>
+<body>
+    <a href="/TP1/hello.php?message=Hello">Afficher Hello</a>
+    <a href="?size=30">en 30</a>
+  
+    
+<?php
+echo "<div style='font-size: {$size}px;'>Message de taille {$size}px en rouge</div>";  
+echo("Hello world !");
+echo "Vous êtes sur la page n°".$_GET["page"];
+ 
+
+  ?>
+    
+
+
+//EX 2 
+
+<div style="font-size: 10px;color:#FF0000">Message de taille 10px en rouge</div>;
+
+<form method="GET">
+    <label for="size">Size : </label>
+    <input type="number" value="" name="size" id="size">
+    <input type="submit" value="Valider">
+</form>
+
+
+// Exo 4
+
+
+<form method="GET" action=""></form>
+    <table border='1'>
+    <tbody>
+        <tr>
+            <td>1-1</td>
+            <td>1-2</td>
+        </tr>
+        <tr>
+            <td>2-1</td>
+            <td>2-2</td>
+        </tr>
+    </tbody>
+</table>
+
+    <input type="color" name="color" value=<?=color
+    
+    
+    
+    </body>
+</html>
